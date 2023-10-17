5c925d78dfdf2c01a301b5e88df0c7378ba948c7
siyoon <siyoon@friendli.ai>
2023-10-17 21:55:46 +0900

    hungry design token system

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   // { href: '/about', title: 'About' },
   { href: '/blog', title: 'Blog' },
-  { href: 'https://github.com/Lee-Si-Yoon', title: 'Github' },
+  // { href: 'https://github.com/Lee-Si-Yoon', title: 'Github' },
   // { href: '/tags', title: 'Tags' },
 ]
 
